bd27ef5c9e4d6255b8cabd4ec3c87afbb3090b2d
tumshar <88685603+tumshar@users.noreply.github.com>
2024-09-03 02:12:27 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-500"> hello niggesh</MaxWidthWrapper>
+  )
+}
